592cb5989ed7aa3dd79cc64e3fe24ddd6c0b975f
shay23b <shay.b@strech.io>
2022-04-14 17:44:10 +0300

    a

--- memphis.rb
+++ memphis.rb
@@ -1,5 +1,5 @@
 class Memphis < Formula
-  desc "Greetings from Memphis - CLI"
+  desc "Greetings from Memphis - CLI  "
   homepage "https://memphis.dev"
   url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.2/mem.tar.gz"
   sha256 "58d0015e809bcd2e56a857a9fe56d893e35d1e2d5e1e2dd87e9c73ff0ff63487"
