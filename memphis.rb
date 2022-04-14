8a55cad814708d1fb7451cf8ba07014bf3fa86d5
shay23b <shay.b@strech.io>
2022-04-14 16:54:35 +0300

    added version

--- memphis.rb
+++ memphis.rb
@@ -3,6 +3,7 @@ class Memphis < Formula
   homepage "https://memphis.dev"
   url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.2/mem.tar.gz"
   sha256 "a3decc8af58b34e4a1009452be71c67c907a96d5fc0165d098aa70f3d654af7e"
+  version "v0.1.2"
   license "Apache-2.0"
 
   def install
