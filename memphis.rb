5c0bf5dc4631ae98870d634b9f4a5ac8420413ec
idanasulinStrech <idan@memphis.dev>
2022-05-03 22:29:26 +0300

    version update

--- memphis.rb
+++ memphis.rb
@@ -1,9 +1,9 @@
 class Memphis < Formula
   desc "Greetings from Memphis - CLI  "
   homepage "https://memphis.dev"
-  url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.7/mem.tar.gz"
-  sha256 "cf70f6b7944268931bed064f70ad0ac4c1f53ee1cd102b72170b3bcb4131e487"
-  version "v0.1.7"
+  url "https://github.com/Memphis-OS/memphis-cli/releases/download/v0.1.8/mem.tar.gz"
+  sha256 "119004019202b41dbe4ec5992d598cba751f15bd78ebae50204e27b6d08b951f"
+  version "v0.1.8"
   license "Apache-2.0"
 
   def install
