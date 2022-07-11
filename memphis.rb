0e17d26ceda0d548304db7527f7a0b6ab385c4da
idanasulinStrech <idan@memphis.dev>
2022-07-11 18:00:37 +0300

    version update

--- memphis.rb
+++ memphis.rb
@@ -2,7 +2,7 @@ class Memphis < Formula
   desc "Greetings from Memphis.dev - CLI  "
   homepage "https://memphis.dev"
   url "https://github.com/memphisdev/memphis-cli/releases/download/v0.3.6/mem.tar.gz"
-  sha256 "f14fbaff622edc7dc57db9a075453e5cae9873209db031f892f210f458b63f88"
+  sha256 "f8d2f700d2993762ac60b043f293ced8a2c14309d0f5bc8ac585367ff81dbf08"
   version "v0.3.6"
   license "GPL-3.0"
 
